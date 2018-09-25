5775c3f7d016f2d51f8aa20677729b4baace5fe3
aryrachmadis <43523730+aryrachmadis@users.noreply.github.com>
2018-09-25 17:10:08 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,3 +1,12 @@
+Apa hobi Anda?<br />
+<form action="#" method="POST">
+	<input type="checkbox" name="hobi[]" value="Main Bola">Main Bola<br />
+	<input type="checkbox" name="hobi[]" value="Jajan">Jajan<br />
+	<input type="checkbox" name="hobi[]" value="Main Game">Main Game<br />
+	<input type="checkbox" name="hobi[]" value="Travelling">Travelling<br />
+	<input type="submit" value="Kirim">
+</form>
+
 <?php
 	if (isset($_POST['submit'])) {
 		$gambar = $_FILES['gambar']['name'];
